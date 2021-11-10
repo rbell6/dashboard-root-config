3dd240a96ba04ee97327308951426d2e5dacc004
Richard Bell <rbell@adobe.com>
2021-11-09 21:35:40 -0700

    import map json

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,35 @@
-const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const { merge } = require('webpack-merge');
+const singleSpaDefaults = require('webpack-config-single-spa');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "apocryphal";
-  const defaultConfig = singleSpaDefaults({
-    orgName,
-    projectName: "root-config",
-    webpackConfigEnv,
-    argv,
-    disableHtmlGeneration: true,
-  });
+	const orgName = 'apocryphal';
+	const defaultConfig = singleSpaDefaults({
+		orgName,
+		projectName: 'root-config',
+		webpackConfigEnv,
+		argv,
+		disableHtmlGeneration: true,
+	});
 
-  return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
-    plugins: [
-      new HtmlWebpackPlugin({
-        inject: false,
-        template: "src/index.ejs",
-        templateParameters: {
-          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
-          orgName,
-        },
-      }),
-    ],
-  });
+	return merge(defaultConfig, {
+		// modify the webpack config however you'd like to by adding to this object
+		plugins: [
+			new HtmlWebpackPlugin({
+				inject: false,
+				template: 'src/index.ejs',
+				templateParameters: {
+					isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+					orgName,
+				},
+			}),
+			new CopyWebpackPlugin({
+				patterns: [
+					{from: 'assets', to: 'assets'},
+				]
+			})
+		],
+	});
 };
